3a8ae9bbe6b2d1457ff2fcc2ba48383f68c151ee
fermett <emmanuel1078@hotmail.com>
2024-10-17 02:44:05 +0000

    hice mas legible la linea 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let who = ["Mi abuela", "Mi perro", "Mi novia", "El cometareas"];
 let action = ["se comio", "se pateo", "lo golpearon en la ingle con un coco"];
-let action2 = ["mi tarea", "mi carro", "mi casa"];
+let what = ["mi tarea", "mi carro", "mi casa"];
 let when = ["en la mañana", "en la noche", "en la madrugada"];
 
 function getRandomIndex(array) {
@@ -10,10 +10,10 @@ function getRandomIndex(array) {
 function generarFrase() {
   let randomWho = who[getRandomIndex(who)];
   let randomAction = action[getRandomIndex(action)];
-  let randomAction2 = action2[getRandomIndex(action2)];
+  let randomWhat = what[getRandomIndex(what)];
   let randomWhen = when[getRandomIndex(when)];
   let resultado =
-    randomWho + " " + randomAction + " " + randomAction2 + " " + randomWhen;
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
   document.getElementById("excusa").innerText = resultado;
 }
 
